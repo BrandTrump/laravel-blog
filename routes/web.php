00abb141508af337a272324a2261aa77cb309c26
BrandTrump <brandon.trump@rocketmail.com>
2021-04-14 16:32:42 +1200

    Added route wildcard constraints

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,8 @@ Route::get('posts/{post}', function ($slug) {
 
     $path = __DIR__ . "/../resources/posts/{$slug}.html";
 
+    //ddd($path);
+
     if (! file_exists($path)) {
         return redirect('/');
        // abort(404);
@@ -32,4 +34,4 @@ Route::get('posts/{post}', function ($slug) {
     return view('post', [
         'post' => $post
     ]);
-});
+})->where('post', '[A-z_\-]+');
