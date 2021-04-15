33015ef6f51c227eff0c5f6945d9ccc1fdc8bf9e
BrandTrump <brandon.trump@rocketmail.com>
2021-04-16 09:39:24 +1200

    Created blade layout

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+
+        <title>My Blog</title>
+        <link rel="stylesheet" href="/app.css">
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    </head>
+
+    <body>
+        @yield('content')
+    </body>
+
+</html>
