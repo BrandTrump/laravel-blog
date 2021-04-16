39802eb4ba30c943317a2c05b8e82e8903d15eba
BrandTrump <brandon.trump@rocketmail.com>
2021-04-16 13:13:58 +1200

    Created reach out for layout files

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,29 +1,13 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('components.layout')
 
-        <title>My Blog</title>
+@section('content')
+    <article>
+        <h1>{{ $post->title }}</h1>
 
-        <link rel="stylesheet" href="/app.css">
+        <div>
+            {!! $post->body !!}
+        </div>
+    </article>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    </head>
-
-    <body>
-
-        <article>
-            <h1>{{ $post->title }}</h1>
-
-            <div>
-                {!! $post->body !!}
-            </div>
-        </article>
-
-        <a href="/">Go Back</a>
-
-    </body>
-</html>
+    <a href="/">Go Back</a>
+@endsection
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,33 +1,16 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<x-layout>
+    @foreach ($posts as $post)
+        <article>
+            <a href="/posts/{{ $post->slug }}">
+                {{ $post->title }}
+            </a>
 
-        <title>My Blog</title>
+            <h1></h1>
 
-        <link rel="stylesheet" href="/app.css">
+            <div>
+                {{ $post->excerpt }}
+            </div>
+        </article>
+    @endforeach
+</x-layout>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    </head>
-
-    <body>
-
-        @foreach ($posts as $post)
-            <article>
-                <a href="/posts/{{ $post->slug }}">
-                    {{ $post->title }}
-                </a>
-
-                <h1></h1>
-
-                <div>
-                    {{ $post->excerpt }}
-                </div>
-            </article>
-        @endforeach
-
-    </body>
-</html>
